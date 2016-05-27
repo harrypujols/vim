65e54e6b17df9d4214b7feebecad68d310703efe
Harry Pujols <harrypujols@me.com>
2016-05-27 01:15:59 -0400

    deployment script

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # vim
 vim settings to carry around
+
+## to deploy
+- Run `npm install`
+- Then `gulp`
+
+The .vimrc file should go where it should be.
+
+## after deploying
+- Install plugins `vim +PluginInstall`
+- Remember to get fonts from https://github.com/powerline/fonts
+- Profit
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
-var gulp   = require('gulp'),
-var rename = require('gulp-rename');
+var gulp        = require('gulp'),
+    rename      = require('gulp-rename');
 
 gulp.task('move', function() {
-  gulp.src('./vimrc')
-    .pipe(rename('.vimrc'))                                                                          .pipe(gulp.dest('~/'));
-}
+  gulp.src('./vimrc.txt')
+    .pipe(rename('.vimrc'))
+    .pipe(gulp.dest('~/'))
+});
 
 gulp.task('default', ['move']);
