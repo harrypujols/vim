b5f5955e629e3c740b2cbe9a4c54c83df9bf5664
Harry Pujols <harrypujols@me.com>
2016-06-29 22:45:02 -0400

    cleaning up python script

--- install.py
+++ install.py
@@ -6,23 +6,26 @@ def install():
     print "[2] Cousine for Powerline 16 LINUX"
     print "[3] Menlo 14"
     font = raw_input("").lower()
-    macfont = r"set guifont=Inconsolata\\ for\\ Powerline:h16"
-    linuxfont = r"set guifont=Cousine\ for\ Powerline\ 16"
+
+    macfont = r"Inconsolata\\ for\\ Powerline:h16"
+    linuxfont = r"Cousine\ for\ Powerline\ 16"
+
+    os.system("rsync -av ./vimrc ~/.vimrc")
+
     if font == "1":
-        os.system("rsync -av ./vimrc ~/.vimrc")
-        os.system("sed -i '' 's/set guifont=Menlo:h14/"+ macfont +"/g' ~/.vimrc")
+        os.system("sed -i '' 's/Menlo:h14/"+ macfont +"/g' ~/.vimrc")
         os.system("sed -i '' '/\" start clean/,/\" end clean/d' ~/.vimrc")
-        os.system("vim +PuginInstall +qall")
+        print "Inconsolata 16 set as default GUI font"
     elif font == "2":
-        os.system("rsync -av ./vimrc ~/vimrc")
-        os.system("sed -i 's/set guifont=Menlo:h14/"+ linuxfont +"/g' ~/.vimrc")
+        os.system("sed -i 's/Menlo:h14/"+ linuxfont +"/g' ~/.vimrc")
         os.system("sed -i '/\" start clean/,/\" end clean/d' ~/.vimrc")
-        os.system("vim +PuginInstall +qall")
+        print "Cousine 16 set as default GUI font"
     elif font == "3":
-        os.system("cp -i ./vimrc ~/.vimrc")
-        os.system("vim +PuginInstall +qall")
+        print "Menlo 14 set as default GUI font"
     else:
-        print "select 1, 2 or 3"
+        print "invalid selection"
         install()
+    
+    os.system("vim +PuginInstall +qall")
 
 install()
